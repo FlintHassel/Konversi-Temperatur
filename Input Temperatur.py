30f536738a132ab231878260fad0f062396bfe6f
Akmal Khairan Rizaldi <akmalcode843@gmail.com>
2025-03-06 08:04:48 +0700

    Add files via upload

---
+++ Input Temperatur.py
@@ -0,0 +1,7 @@
+print( "konversi temperatur")
+celcius = float(input("Masukan Suhu Dalam Celcius = "))
+print("Suhu Adalah = ", celcius, "C")
+#Fahrenheit
+
+Farenheit = ((5/9) * celcius) + 32
+print ("Suhu Dalam Fahrenheit = " ,Farenheit , "F" )
